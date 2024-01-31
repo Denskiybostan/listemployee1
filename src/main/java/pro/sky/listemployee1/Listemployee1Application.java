2e98c8cd5247449b3e8398e0f2046e758332c717
denis <deniz246@mail.ru>
2024-01-31 20:31:01 +0300

    1

---
+++ src/main/java/pro/sky/listemployee1/Listemployee1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.listemployee1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Listemployee1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Listemployee1Application.class, args);
+	}
+
+}
